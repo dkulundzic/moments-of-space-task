8ff61dfd60d87a70ef6ad92d96328d04f73d0a0b
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2023-09-07 09:37:46 +0200

    Initial Commit

---
+++ MomentsOfSpace/MomentsOfSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsOfSpaceApp.swift
+//  MomentsOfSpace
+//
+//  Created by Domagoj Kulundzic on 07.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct MomentsOfSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
